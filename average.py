7d8dbfb6b682757b2c54391b2f49961432cf9e65
chandanchanchal <34713852+chandanchanchal@users.noreply.github.com>
2019-09-27 14:46:02 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,19 @@
+def average(list)
+	''' Find average of a list of numvers '''
+	return sum(list) / float(len(list))
+	
+class FilterModuel(object):
+     ''' Query filter '''
+	 def filters(self):
+		 return {
+		      ' average': average
+		 } 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
