2a3180ebba4c9744823eb05f6f24b20d695a3ee0
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-01-06 10:10:21 +0530

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,3 +3,7 @@ console.log("hello js");
 const array = [1, 2, 3, 4];
 
 console.log(array, "array");
+setTimeout(() => {
+  console.log("this message is delayed by 2 seconds");
+}, 2000);
+console.log("this is the last line of the sync code");
