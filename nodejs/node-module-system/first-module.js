cca934a74a82aa58e005e207da8f74df8f857f49
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-01-06 10:24:17 +0530

    Update first-module.js

--- nodejs/node-module-system/first-module.js
+++ nodejs/node-module-system/first-module.js
@@ -2,20 +2,7 @@ function add(a, b) {
   return a + b;
 }
 
-function substract(a, b) {
-  return a - b;
-}
-
-function divide(a, b) {
-  if (b === 0) {
-    throw new Error("Divide by zero is not allowed");
-  }
-
-  return a / b;
-}
 
 module.exports = {
-  add,
-  substract,
-  divide,
+  add  
 };
