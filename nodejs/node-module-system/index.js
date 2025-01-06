d01e11525770f5ac2f8ac6a88725eb2ef7d93c7e
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-01-06 10:23:16 +0530

    Update index.js

--- nodejs/node-module-system/index.js
+++ nodejs/node-module-system/index.js
@@ -5,10 +5,3 @@ const firstModule = require("./first-module");
 
 console.log(firstModule.add(10, 20));
 
-try {
-  console.log("trying to divide by zero");
-  let result = firstModule.divide(0, 10);
-  console.log(result, "result");
-} catch (error) {
-  console.log("Caught an error", error.message);
-}
