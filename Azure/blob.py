08586117412db63add3d30102974d6d7e736691b
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-04-02 13:26:09 +0530

    Update blob.py

--- Azure/blob.py
+++ Azure/blob.py
@@ -1,16 +1,11 @@
-import logging
 import azure.functions as func
+import logging
 
-def main(req: func.HttpRequest, inputblob: func.InputStream) -> func.HttpResponse:
-    try:
-        # Retrieve the file name from the blob trigger
-        file_name = inputblob.name.split('/')[-1]  # Extract the file name
-        
-        logging.info(f"File uploaded: {file_name}")
-        
-        # Return the file name as a response (just for confirmation)
-        return func.HttpResponse(f"File uploaded: {file_name}", status_code=200)
-    
-    except Exception as e:
-        logging.error(f"Error: {str(e)}")
-        return func.HttpResponse("Error processing the file", status_code=500)
+app = func.FunctionApp()
+
+@app.blob_trigger(arg_name="myblob", path="images/{name}",
+                               connection="ckcazlearningwithfun1_STORAGE") 
+def BlobTrigger1(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob"
+                f"Name: {myblob.name}"
+                f"Blob Size: {myblob.length} bytes")
